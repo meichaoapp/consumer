553d590545d0c8dedde646faf218421412a77d19
bjsonghongxu <sleeponthestar@163.com>
2018-10-10 13:39:13 +0800

    want submit

--- pages/ucenter/desire/desire.js
+++ pages/ucenter/desire/desire.js
@@ -9,6 +9,7 @@ Page({
   data: {
     array: ['请选择反馈类型', '商品相关', '物流状况', '客户服务', '优惠活动', '功能异常', '产品建议', '其他'],
     index: 0,
+    content:"",
   },
   bindPickerChange: function (e) {
     console.log('picker发送选择改变，携带值为', e.detail.value)
@@ -17,6 +18,8 @@ Page({
     })
   },
   onLoad: function (options) {
+    this.$wuxToast = app.Wux().$wuxToast
+
   },
   onReady: function () {
 
@@ -30,5 +33,32 @@ Page({
   },
   onUnload: function () {
     // 页面关闭
-  }
+  },
+  //提交
+  submit:function(){
+    var _this = this;
+    if ("" == _this.data.content || _this.data.content == null){
+      _this.$wuxToast.show({ type: 'forbidden', text: "内容不能为空，请填写后提交！", });
+      return;
+    }
+    var data ={
+       content: _this.data.content,
+    }
+    util.request(api.Desire, data,"POST").then(function (res) {
+      if (res.rs === 1) {
+        _this.setData({
+          content: "",
+        });
+        _this.$wuxToast.show({ type: 'success', text: "提交成功!", });
+      }
+    });
+  },
+
+  //绑定愿望
+  bindContent: function (e) {
+    var _this = this;
+    this.setData({
+      content: e.detail.value,
+    })
+  },
 })
