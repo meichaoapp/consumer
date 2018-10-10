7158cae709a94e52986108d9a1779fc2a04506ee
王舒心 <vesna@wangshuxindeMacBook-Pro.local>
2018-10-10 20:47:48 +0800

    “模块我的以及我的优惠券我的参团基本样式搭建”

--- pages/shopping/coupon/coupon.js
+++ pages/shopping/coupon/coupon.js
@@ -1,5 +1,5 @@
-var util = require('../../../utils/util.js');
-var api = require('../../../config/api.js');
+//var util = require('../../../utils/util.js');
+//var api = require('../../../config/api.js');
 
 
 
@@ -16,39 +16,39 @@ Page({
 
   },
   onShow: function () {
-    let that = this;
-    that.getCouponList();
+    // let that = this;
+    // that.getCouponList();
   },
  
 
 
-  ///去使用
-  toUse:function(e){
-    try {
-      wx.setStorageSync('couponId', e.currentTarget.dataset.id);
-    } catch (e) {
+  // ///去使用
+  // toUse:function(e){
+  //   try {
+  //     wx.setStorageSync('couponId', e.currentTarget.dataset.id);
+  //   } catch (e) {
 
-    }
-    //选择该收货地址
-    wx.redirectTo({
-      url: '/pages/shopping/checkout/checkout'
-    })
-  },
+  //   }
+  //   //选择该收货地址
+  //   wx.redirectTo({
+  //     url: '/pages/shopping/checkout/checkout'
+  //   })
+  // },
 
-  //获取优惠券列表
-  getCouponList() {
-    let that = this;
-    util.request(api.CouponList,{
-      isUsed: 0,
-      isTransmit: 0
-    }, 'POST').then(function (res) {
-      if (res.errno === 0) {
-        //console.log(res.data);
-        that.setData({
-          couponList: res.data
-        });
-      }
-    });
-  },
+  // //获取优惠券列表
+  // getCouponList() {
+  //   let that = this;
+  //   util.request(api.CouponList,{
+  //     isUsed: 0,
+  //     isTransmit: 0
+  //   }, 'POST').then(function (res) {
+  //     if (res.errno === 0) {
+  //       //console.log(res.data);
+  //       that.setData({
+  //         couponList: res.data
+  //       });
+  //     }
+  //   });
+  // },
 
 })
