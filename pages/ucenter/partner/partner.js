d17b75cc5a93f302d89b9ee8acb42eb497d12e00
bjsonghongxu <sleeponthestar@163.com>
2018-10-10 20:19:56 +0800

    modify

--- pages/ucenter/partner/partner.js
+++ pages/ucenter/partner/partner.js
@@ -1,13 +1,29 @@
 var util = require('../../../utils/util.js');
 var api = require('../../../config/api.js');
+const user = require('../../../services/user.js');
+const maps = require('../../../utils/maps.js');
 var app = getApp();
 
 Page({
   data: {
-
+    userInfo: {},
+    latitude: 0.00,
+    longitude: 0.00,
+    address: "",	   //地址
+    name :"", //商户姓名
+    phone :"", //电话
   },
 
   onLoad: function (options) {
+    let userInfo = wx.getStorageSync('userInfo');
+    let token = wx.getStorageSync('token');
+    this.$wuxToast = app.Wux().$wuxToast
+
+    if(null == userInfo){
+      wx.redirectTo({
+        url: '/pages/auth/login/login'
+      });
+    }
   },
   onReady: function () {
 
@@ -21,5 +37,28 @@ Page({
   },
   onUnload: function () {
     // 页面关闭
-  }
+  },
+  //提交
+  submit: function () {
+    var _this = this;
+    // if ("" == _this.data.content || _this.data.content == null) {
+    //   _this.$wuxToast.show({ type: 'forbidden', text: "内容不能为空，请填写后提交！", });
+    //   return;
+    // }
+    var data = {
+      latitude: _this.data.latitude,
+      longitude: _this.data.longitude,
+      address: _this.data.address,
+      name: _this.data.name,
+      phone: _this.data.phone,
+    }
+    util.request(api.Partner, data, "POST").then(function (res) {
+      if (res.rs === 1) {
+        _this.setData({
+          content: "",
+        });
+        _this.$wuxToast.show({ type: 'success', text: "提交成功!", });
+      }
+    });
+  },
 })
