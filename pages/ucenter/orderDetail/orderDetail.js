54669da4c004ba73a42a0591f9cb2c5a5fae09a3
bjsonghongxu <bjsonghongxu@corp.netease.com>
2018-10-15 23:15:24 +0800

    Update pages/ucenter/orderDetail/orderDetail.js
    modify

--- pages/ucenter/orderDetail/orderDetail.js
+++ pages/ucenter/orderDetail/orderDetail.js
@@ -3,34 +3,47 @@ var api = require('../../../config/api.js');
 
 Page({
   data: {
-    orderId: 0,
-    orderInfo: {},
-    orderGoods: [],
-    handleOption: {}
+    orderId:0,
+    status: 0, // 订单状态 0 待支付 1 已支付 2 待领取 3 已完成 4 放弃 5 退货
+    totalPay: 0.00,//共付
+    needPay: 0.00,// 应付
+    orderQRcode: "",// 订单二维码（包含订单id）
+    code: "", //订单识别码
+    orderNums: "", //订单数量
+    groupPurchase:{},
+    merchant:{},
+    goodsList: [],
   },
   onLoad: function (options) {
     // 页面初始化 options为页面跳转所带来的参数
     this.setData({
       orderId: options.id
     });
-    //this.getOrderDetail();
+    this.getOrderDetail();
   },
-  // getOrderDetail() {
-  //   let that = this;
-  //   util.request(api.OrderDetail, {
-  //     orderId: that.data.orderId
-  //   }).then(function (res) {
-  //     if (res.errno === 0) {
-  //       console.log(res.data);
-  //       that.setData({
-  //         orderInfo: res.data.orderInfo,
-  //         orderGoods: res.data.orderGoods,
-  //         handleOption: res.data.handleOption
-  //       });
-  //       //that.payTimer();
-  //     }
-  //   });
-  // },
+  getOrderDetail() {
+    let that = this;
+    util.request(api.QueryOrderDetail, {
+      orderId: that.data.orderId
+    },"POST").then(function (res) {
+      if (res.rs == 1) {
+     //   console.log(res.data);
+        that.setData({
+          orderId: res.data.id, //订单ID
+          status: res.data.status, // 订单状态 0 待支付 1 已支付 2 待领取 3 已完成 4 放弃 5 退货
+          totalPay: res.data.totalPay,//共付
+          needPay: res.data.needPay,// 应付
+          orderQRcode: res.data.orderQRcode,// 订单二维码（包含订单id）
+          code: res.data.code, //订单识别码
+          orderNums: res.data.orderNums,
+          merchant: res.data.merchant,
+          groupPurchase: res.data.groupPurchase,
+          goodsList: res.data.goodsList
+        });
+        //that.payTimer();
+      }
+    });
+  },
   // payTimer() {
   //   let that = this;
   //   let orderInfo = that.data.orderInfo;
