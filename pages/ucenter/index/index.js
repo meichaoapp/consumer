1e77a6e6a448b31691270d43fcb57860d27bdb5f
wangyuechao <1184624439@qq.com>
2018-10-27 10:50:19 +0800

    在线联系

--- pages/ucenter/index/index.js
+++ pages/ucenter/index/index.js
@@ -5,7 +5,8 @@ var app = getApp();
 
 Page({
   data: {
-    userInfo: {}
+    userInfo: {},
+    isShowContactBox: true
   },
   onLoad: function (options) {
     // 页面初始化 options为页面跳转所带来的参数
@@ -59,6 +60,18 @@ Page({
     //   }
     // })
     
+  },
+  toShowContactBox() {
+    wx.hideTabBar({}) 
+    this.setData({
+      isShowContactBox: false
+    });
+  },
+  toHideContactBox() {
+    wx.showTabBar({}) 
+    this.setData({
+      isShowContactBox: true
+    });
   },
   goLogin(){
     wx.navigateTo({
