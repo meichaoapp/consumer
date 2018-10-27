b413c5ca08858e639cc19edef9f4095641e7697b
bjsonghongxu <bjsonghongxu@corp.netease.com>
2018-10-27 12:20:34 +0800

    优化

--- pages/shopping/dollarTreasureDetail/dollarTreasureDetail.js
+++ pages/shopping/dollarTreasureDetail/dollarTreasureDetail.js
@@ -124,7 +124,6 @@ Page({
         console.log("detail.status : " + data.detail.status );
         that.setData({
           detail: data.detail, //详情
-          joinHistories: data.joinHistories,//参与历史
         });
         that.countDown();
       }
