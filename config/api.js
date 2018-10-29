8ed9055023fd5bcb9e2ed76f9994b70f3364aa9c
bjsonghongxu <bjsonghongxu@corp.netease.com>
2018-10-29 10:07:38 +0800

    优化

--- config/api.js
+++ config/api.js
@@ -1,6 +1,6 @@
-var NewApiRootUrl = 'http://localhost:8080/api/';
+//var NewApiRootUrl = 'http://localhost:8080/api/';
 var NewApiRootUrl = "https://wxapp.galaxyxd.com/api/";
-var NewApiRootUrl = 'http://101.201.213.248:8093/api/';
+//var NewApiRootUrl = 'http://101.201.213.248:8093/api/';
 module.exports = {
   WXLogin: NewApiRootUrl + 'consumer/user/login',//微信登录
 
