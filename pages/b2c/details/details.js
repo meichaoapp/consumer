e6d380aad021e33e03a19e3cdc6ece1a013005c4
liqi <liqijr@feinno.com>
2019-03-29 23:05:40 +0800

    电商详情选择规格弹框添加

--- pages/b2c/details/details.js
+++ pages/b2c/details/details.js
@@ -78,5 +78,47 @@ Page({
                 })
             }
         });
-    }
+    },
+    //打开规格弹窗
+    openSpecsBox: function () {
+        let that = this;
+        // 显示遮罩层
+        var animation = wx.createAnimation({
+            duration: 200,
+            timingFunction: "linear",
+            delay: 0
+        })
+        that.animation = animation
+        animation.translateY(300).step()
+        that.setData({
+            animationData: animation.export(),
+            specsStatus: true
+        })
+        setTimeout(function () {
+            animation.translateY(0).step()
+            that.setData({
+                animationData: animation.export()
+            })
+        }.bind(this), 200)
+    },
+    hideSpecsBox: function () {
+        // 隐藏遮罩层
+        var animation = wx.createAnimation({
+            duration: 200,
+            timingFunction: "linear",
+            delay: 0
+        })
+        this.animation = animation
+        animation.translateY(300).step()
+        this.setData({
+            animationData: animation.export(),
+        })
+        setTimeout(function () {
+            animation.translateY(0).step()
+            this.setData({
+                animationData: animation.export(),
+                specsStatus: false,
+            })
+        }.bind(this), 200)
+    },
 })
